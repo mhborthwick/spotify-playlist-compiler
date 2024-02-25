434dafcb392a76e80157c32dfce4decc07e1b3e0
Mike Borthwick <mhborthwick@gmail.com>
2024-02-25 01:32:24 -0800

    add basic oauth

--- proxy/cmd/main.go
+++ proxy/cmd/main.go
@@ -6,11 +6,31 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/mhborthwick/spotify-playlist-compiler/.config"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/spotify"
 )
 
+var authConfig *oauth2.Config
+
+func GetRandomString() string {
+	return uuid.NewString()
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello!")
+	fmt.Fprint(w, "hey")
+}
+
+func Authorize(w http.ResponseWriter, r *http.Request) {
+	state := GetRandomString()
+	http.Redirect(w, r, authConfig.AuthCodeURL(state), http.StatusSeeOther)
+}
+
+func Callback(w http.ResponseWriter, r *http.Request) {
+	code := r.FormValue("code")
+	token, _ := authConfig.Exchange(r.Context(), code)
+	fmt.Fprint(w, token.AccessToken)
 }
 
 func main() {
@@ -18,7 +38,21 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("I'm running on host %s\n", cfg.Id)
+
+	authConfig = &oauth2.Config{
+		ClientID:     cfg.Id,
+		ClientSecret: cfg.Secret,
+		RedirectURL:  "http://localhost:1337/callback",
+		Endpoint:     spotify.Endpoint,
+		Scopes: []string{
+			"user-read-email",
+			"user-read-private",
+		},
+	}
+
 	http.HandleFunc("/", Handler)
+	http.HandleFunc("/login", Authorize)
+	http.HandleFunc("/callback", Callback)
+
 	log.Fatal(http.ListenAndServe(":1337", nil))
 }
--- proxy/go.mod
+++ proxy/go.mod
@@ -1,3 +1,12 @@
 module github.com/mhborthwick/spotify-playlist-compiler/proxy
 
 go 1.21.1
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/oauth2 v0.17.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
