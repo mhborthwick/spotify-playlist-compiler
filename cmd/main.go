2a4caa78098dc44d0482f6af0866988c7dc1580a
Mike Borthwick <mhborthwick@gmail.com>
2024-02-26 23:18:52 -0800

    get spotify uris

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,37 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"regexp"
 
 	"github.com/mhborthwick/spotify-playlist-compiler/.config"
 )
+
+type Track struct {
+	URI string `json:"uri"`
+}
+
+type GetPlaylistResponse struct {
+	Items []struct {
+		Track Track `json:"track"`
+	} `json:"items"`
+}
+
+func GetSpotifyURIs(body []byte) ([]string, error) {
+	var parsed GetPlaylistResponse
+	if err := json.Unmarshal(body, &parsed); err != nil {
+		fmt.Println("Error")
+		return nil, err
+	}
+	uris := make([]string, len(parsed.Items))
+	for i, item := range parsed.Items {
+		uris[i] = item.Track.URI
+	}
+	return uris, nil
+}
 
 func main() {
 	cfg, err := config.LoadFromPath(context.Background(), "pkl/local/config.pkl")
@@ -15,7 +40,16 @@ func main() {
 		panic(err)
 	}
 
-	id := "4KMuVswhHsgHusA1hSdZQ5"
+	playlist := "https://open.spotify.com/playlist/4KMuVswhHsgHusA1hSdZQ5?si=a4b8123f214d470c"
+
+	re := regexp.MustCompile(`[a-zA-Z0-9]{22}`)
+
+	id := re.FindString(playlist)
+
+	if id == "" {
+		fmt.Printf("Invalid playlist")
+		return
+	}
 
 	url := "https://api.spotify.com/v1/playlists/" + id + "/tracks"
 	req, err := http.NewRequest("GET", url, nil)
@@ -47,5 +81,12 @@ func main() {
 		return
 	}
 
-	fmt.Println(string(body))
+	uris, err := GetSpotifyURIs(body)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(uris)
 }
