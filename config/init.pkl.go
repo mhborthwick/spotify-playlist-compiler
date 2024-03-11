573985e696aae7d0e186231d388550dce151ec80
Mike Borthwick <mhborthwick@gmail.com>
2024-03-10 23:20:01 -0700

    remove config

--- config/init.pkl.go
+++
@@ -1,8 +0,0 @@
-// Code generated from Pkl module `mhborthwick.spotify.playlist.compiler.Config`. DO NOT EDIT.
-package config
-
-import "github.com/apple/pkl-go/pkl"
-
-func init() {
-	pkl.RegisterMapping("mhborthwick.spotify.playlist.compiler.Config", Config{})
-}
